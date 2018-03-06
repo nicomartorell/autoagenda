9970ee2c02efd5780499c5983c717d26f8bebfe5
nicomartorell <martorellnicolas1995@hotmail.com>
2018-03-05 22:54:21 -0300

    Comentario

--- src/prueba/Escuela.java
+++ src/prueba/Escuela.java
@@ -8,8 +8,8 @@ public class Escuela {
 	private ArrayList<BloqueAsignable> matriz;
 	
 	public Escuela() {
-		matriz = new ArrayList<BloqueAsignable>();
-	}
+		matriz = new ArrayList<BloqueAsignable>(); //obviamente falta llenar esta matriz, pasar algunas cosas que están en la clase
+	}						// Test para acá, pero bueno, esto al menos da una idea de cómo sería esta clase
 	
 	public void calcular(BloqueAsignable data) {
 		if(data.equals(matriz.get(matriz.size()-1))) {
