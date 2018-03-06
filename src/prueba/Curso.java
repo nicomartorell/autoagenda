b0ef895ee43770966520e4c413b798c41ce3a886
nicomartorell <martorellnicolas1995@hotmail.com>
2018-03-05 22:56:12 -0300

    comentario

--- src/prueba/Curso.java
+++ src/prueba/Curso.java
@@ -2,7 +2,8 @@ package prueba;
 
 import java.util.ArrayList;
 
-public class Curso {
+public class Curso { // se me ocurre que tiene sentido que la clase curso tenga un Array de bloques, y la Grilla tenga un Array de Cursos
+			// más que nada porque así escribí la función calcular, pero se puede ver qué es mejor
 	ArrayList<Bloque> bloques;
 	
 	public Curso(int bloques) {
