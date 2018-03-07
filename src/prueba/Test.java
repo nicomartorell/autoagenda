de738174a901c4bb603cd755e46df4f816ea1a0d
cliviero <31523336+cliviero@users.noreply.github.com>
2018-03-07 11:44:24 -0300

    Update Test.java
    -Se borraron los métodos que trataban a la Grilla como una matriz y no como una lista de cursos
    -Se simplifico la forma de agregar BloquesAsignables nuevos al ArrayList
    -Se crearon algunos elementos para probar con una grilla

--- src/prueba/Test.java
+++ src/prueba/Test.java
@@ -7,92 +7,33 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int cantCursos = 3;
-		int cantBloques = 6; // bloques por día
-		int cantMaterias = cantBloqueMateria.length;
-		int cantBloqueMateria[] = { 3, 6, 3, 3, 3 }; // cantidad de bloques asignado a cada materia
-		// por ahora se toma numeros que puedan dividirse para la cantidad de cursos
-		int cantMaterias = cantBloqueMateria.length; // redundante? si ya tengo cantBloqueMateria puedo usar .length de esa variable
-		
 		ArrayList<BloqueAsignable> datos = new ArrayList<BloqueAsignable>();
-		datos = crearBloquesAsignables("Pedrito", "A", "Matematica", "2");
+		datos.add(new BloqueAsignable("Pedrito", "A", "Matematica", "2"));
+		datos.add(new BloqueAsignable("Claudia", "A", "Fisica", "2"));
+		datos.add(new BloqueAsignable("Eduardo", "A", "Lengua", "2"));
+		datos.add(new BloqueAsignable("Pedrito", "B", "Biologia", "1"));
+		datos.add(new BloqueAsignable("Claudia", "B", "Quimica", "3"));
+		datos.add(new BloqueAsignable("Eduardo", "B", "Sociales", "2"));
+		datos.add(new BloqueAsignable("Pedrito", "C", "Biologia", "3"));
+		datos.add(new BloqueAsignable("Claudia", "C", "Quimica", "1"));
+		datos.add(new BloqueAsignable("Eduardo", "C", "Sociales", "2"));
 		
 		Escuela escuela = new Escuela(datos);
-		escuela.calcular(datos.get(0));
+		//antes de calcular habria que controlar los datos ingresados
 		
-		Materia materias[] = new Materia[cantMaterias]; //no debería ser 5? usamos cantMaterias acá
-		materias[0] = new Materia("A");
-		materias[1] = new Materia("B");
-		materias[2] = new Materia("C");
-		materias[3] = new Materia("D");
-		materias[4] = new Materia("E");
-
-		int cantBloqueMateriaPorCurso[] = calcularCantBloqueMateriaPorCurso(cantBloqueMateria, cantCursos);
-		// cantidad de bloques asignado a cada materia por curso
-		// aca el array quedaria = { 1, 2, 1, 1 ,1};
+		//diria que lo controle la escuela
 		
-		Grilla grilla[][] = new Grilla[cantBloques][cantCursos]; // esto está muy bien
-		grilla = inicializarGrilla(); //estaría bueno que este método sea de la clase Grilla
-
-		// Todavia no se contemplan los profesores
-
-		if (cantCursos * cantBloques >= sumarBloques(cantBloqueMateria)) {
+		//antes lo "controlabamos" asi
+		
+		/*if (cantCursos * cantBloques >= sumarBloques(cantBloqueMateria)) {
 			Grilla mejorGrilla[][] = procesarGrilla(grilla, materias);
 			mostrarGrilla(mejorGrilla);
 		} else {
 			System.out.println("La suma de los bloques es mayor a la disponible en la grilla");
-		}
-
-	}
-
-	private static Grilla[][] inicializarGrilla() {
-		Grilla grilla[][] = { null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-				null, null, null };
-		return grilla;
-	}
-
-	private static Grilla[][] mostrarGrilla(Grilla[][] mejorGrilla) {
-		for (int i = 0; i < mejorGrilla.length; i++) {
-			System.out.print("|");
-			for (int j = 0; j < mejorGrilla[j].length; j++) {
-				System.out.print(mejorGrilla[i][j]);
-			}
-			System.out.println("|");
-		}
-		return mejorGrilla;
-	}
-
-	private static Grilla[][] procesarGrilla(Grilla grilla[][], Materia materias[]) {
-		Grilla mejorGrilla[][] = inicializarGrilla();
-		int i = 0;
-		int j = 0;
-		// aca va la manija
-		while (i < mejorGrilla.length) {
-			i++;
-			while (j < mejorGrilla[i].length) {
-				j++;
-			}
-		}
-		return mejorGrilla;
-	}
-
-	private static int[] calcularCantBloqueMateriaPorCurso(int[] cantBloqueMateria, int cantCursos) {
-		int cantBloqueMateriaPorCurso[] = { 0, 0, 0, 0, 0 };
-		for (int i = 0; i < cantBloqueMateria.length; i++) {
-			cantBloqueMateriaPorCurso[i] = cantBloqueMateria[i] / cantCursos;
-		}
-		return cantBloqueMateriaPorCurso;
-	}
-
-	private static int sumarBloques(int cantBloqueMaterias[]) {
-		int suma = 0;
-		for (int i = 0; i < cantBloqueMaterias.length; i++) {
-			suma += cantBloqueMaterias[i];
-		}
-		return suma;
+		}*/
+		
+		escuela.calcular(datos.get(0));
+		
 	}
 	
-	private ArrayList<BloqueAsignable> crearBloquesAsignables (Profesor p, Curso c, Materia m, int cantiBloque);
-	BloqueAsignable bloque = new BloqueAsignable (p, c, m, cantiBloque);
-	datos.add(bloque);
 }
