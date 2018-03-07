8141ee0b6f31efbb9f619ec58061f702ca082328
cliviero <31523336+cliviero@users.noreply.github.com>
2018-03-07 00:49:59 -0300

    Se modifico la clase Escuela

--- src/prueba/Escuela.java
+++ src/prueba/Escuela.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 
 public class Escuela {
 	
-	private ArrayList<BloqueAsignable> matriz;
+	private ArrayList<BloqueAsignable> datos; //datos para crear una grilla
+	private ArrayList<Grilla> grillas;
 	
-	public Escuela() {
-		matriz = new ArrayList<BloqueAsignable>(); //obviamente falta llenar esta matriz, pasar algunas cosas que están en la clase
-	}						// Test para acá, pero bueno, esto al menos da una idea de cómo sería esta clase
+	public Escuela(ArrayList<BloqueAsignable> datos) {
+		this.datos = datos; 	//obviamente falta llenar esta matriz, pasar algunas cosas que están en la clase
+	}				// Test para acá, pero bueno, esto al menos da una idea de cómo sería esta clase
 	
 	public void calcular(BloqueAsignable data) {
 		if(data.equals(matriz.get(matriz.size()-1))) {
