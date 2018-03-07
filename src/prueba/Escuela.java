0d7e05da907b48519ef1f92bf1dae2a4b0762b24
cliviero <31523336+cliviero@users.noreply.github.com>
2018-03-07 11:47:07 -0300

    Update Escuela.java
    -Se agrego el comentario para controlar los datos ingresados

--- src/prueba/Escuela.java
+++ src/prueba/Escuela.java
@@ -9,8 +9,10 @@ public class Escuela {
 	private ArrayList<Grilla> grillas;
 	
 	public Escuela(ArrayList<BloqueAsignable> datos) {
-		this.datos = datos; 	//obviamente falta llenar esta matriz, pasar algunas cosas que están en la clase
-	}				// Test para acá, pero bueno, esto al menos da una idea de cómo sería esta clase
+		this.datos = datos;
+	}				
+	
+	//antes de calcular deberiamos controlar los datos ingresados
 	
 	public void calcular(BloqueAsignable bloqueA) {
 		if(bloqueA == null) { // caso base
