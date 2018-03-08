0083f46eeb642213de0b9f12c67551c461f77397
nicomartorell <martorellnicolas1995@hotmail.com>
2018-03-08 12:49:58 -0300

    Agregué campo "nombre"

--- src/prueba/Curso.java
+++ src/prueba/Curso.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 public class Curso { // se me ocurre que tiene sentido que la clase curso tenga un Array de bloques, y la Grilla tenga un Array de Cursos
 			// más que nada porque así escribí la función calcular, pero se puede ver qué es mejor
 	ArrayList<Bloque> bloques;
+	private String nombre;
 	
-	public Curso(int bloques) {
+	public Curso(String nombre, int bloques) {
+		this.nombre = nombre;
+		
 		this.bloques = new ArrayList<Bloque>();
 		
 		for(int i = 0; i < bloques; i++) {
