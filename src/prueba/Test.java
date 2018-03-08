f5c01e281efd251f11db2757f9c976414a1f4ca8
nicomartorell <martorellnicolas1995@hotmail.com>
2018-03-08 12:55:09 -0300

    Agregué mapas de profesores, cursos y materias.

--- src/prueba/Test.java
+++ src/prueba/Test.java
@@ -12,10 +12,30 @@ public class Test {
 		* toma como parámetros objetos (Profesor, Curso, Materia y un int)
 		* En todo caso antes de crear esta lista, habría que crear una lista de profesores, otra de cursos,
 		* Otra de materias y ahi si armar esto.
+		*Eso es lo que hice acá abajo:
 		*/
 		
+		// Me parece que sería más eficiente que estos sean sets o mapas, los cambié a un HashMap
+		// Eso es porque usualmente queremos ir a agarrar un profesor o un curso según su nombre, no
+		// queremos ir pasando uno por uno (eso es lo que vamos a hacer con los bloques asignables)
+		
+		HashMap<Profesor> profesores = new HashMap<>();
+		profesores.put("Pedrito", new Profesor("Pedrito"));
+		profesores.put("Claudia", new Profesor("Claudia"));
+		profesores.put("Eduardo", new Profesor("Eduardo"));
+		
+		HashMap<Curso> cursos = new HashMap<>();
+		cursos.put("A", ew Curso("A"));
+		cursos.put("B", new Curso("B"));
+		cursos.put("C", new Curso("C"));
+		
+		HashMap<Materia> materias = new HashMap<>();
+		materias.put("Matemática", new Materia("Matemática"));
+		materias.put("Lengua", new Materia("Lengua"));
+		materias.put("Biología", new Materia("Biología"));
+		
 		ArrayList<BloqueAsignable> datos = new ArrayList<BloqueAsignable>();
-		datos.add(new BloqueAsignable("Pedrito", "A", "Matematica", "2"));
+		/*datos.add(new BloqueAsignable("Pedrito", "A", "Matematica", "2"));
 		datos.add(new BloqueAsignable("Claudia", "A", "Fisica", "2"));
 		datos.add(new BloqueAsignable("Eduardo", "A", "Lengua", "2"));
 		datos.add(new BloqueAsignable("Pedrito", "B", "Biologia", "1"));
@@ -23,7 +43,19 @@ public class Test {
 		datos.add(new BloqueAsignable("Eduardo", "B", "Sociales", "2"));
 		datos.add(new BloqueAsignable("Pedrito", "C", "Biologia", "3"));
 		datos.add(new BloqueAsignable("Claudia", "C", "Quimica", "1"));
-		datos.add(new BloqueAsignable("Eduardo", "C", "Sociales", "2"));
+		datos.add(new BloqueAsignable("Eduardo", "C", "Sociales", "2"));*/
+		
+		datos.add(new BloqueAsignable(profesores.get("Pedrito"), cursos.get("A"), materias.get("Matemática"), 2));
+		datos.add(new BloqueAsignable(profesores.get("Pedrito"), cursos.get("B"), materias.get("Matemática"), 1));
+		datos.add(new BloqueAsignable(profesores.get("Pedrito"), cursos.get("C"), materias.get("Matemática"), 3));
+		
+		datos.add(new BloqueAsignable(profesores.get("Claudia"), cursos.get("A"), materias.get("Lengua"), 2));
+		datos.add(new BloqueAsignable(profesores.get("Claudia"), cursos.get("B"), materias.get("Lengua"), 3));
+		datos.add(new BloqueAsignable(profesores.get("Claudia"), cursos.get("C"), materias.get("Lengua"), 1));
+		
+		datos.add(new BloqueAsignable(profesores.get("Eduardo"), cursos.get("A"), materias.get("Biología"), 2));
+		datos.add(new BloqueAsignable(profesores.get("Eduardo"), cursos.get("B"), materias.get("Biología"), 2));
+		datos.add(new BloqueAsignable(profesores.get("Eduardo"), cursos.get("C"), materias.get("Biología"), 2));
 		
 		Escuela escuela = new Escuela(datos);
 		//antes de calcular habria que controlar los datos ingresados
